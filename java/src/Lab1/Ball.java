fa8c9cd3c159dee2e1fa872f3d38d3793c5a7c50
Mironova66 <56535388+Mironova66@users.noreply.github.com>
2020-09-07 15:08:10 +0300

    Add files via upload

---
+++ java/src/Lab1/Ball.java
@@ -0,0 +1,27 @@
+package Lab1;
+
+public class Ball {
+    private String color;
+    private int size;
+
+    public Ball(String c, int s) {
+        color = c;
+        size = s;
+    }
+    public Ball() {
+        color = "No color";
+        size = 0;
+    }
+    public void setColor(String c) {
+        this.color = c;
+    }
+    public void setSize(int s) {
+        this.size = s;
+    }
+    public void getColor() {
+
+    }
+
+}
+
+
